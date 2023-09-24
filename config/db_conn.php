1da98510440f51b7bbd0fb48dc6f66f35595d00e
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-09-24 20:05:20 +0700

    Update db_conn.php

--- config/db_conn.php
+++ config/db_conn.php
@@ -2,9 +2,8 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ajax_crud";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $username);
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
